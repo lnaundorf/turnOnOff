05add2dcdeed670a3aecb493549b4fed922ce27e
Leif Naundorf <kontakt@leifnaundorf.de>
2017-05-21 21:14:03 +0200

    Initial Commit

---
+++ static/turnOnOff.js
@@ -0,0 +1,79 @@
+var too = {
+    serverStates: {
+        0: {
+            title: "Turn on",
+            disabled: false,
+            func: function(server_id) {
+                too.executeFunction(server_id, "turnOn", 1);
+            },
+            styleClass: "btn-primary",
+        },
+        1: {
+            title: "Turned on",
+            func: null,
+            styleClass: "btn-warning"
+        },
+        2: {
+            title: "Turn off",
+            func: function(server_id) {
+                too.executeFunction(server_id, "turnOff", 3);
+            },
+            styleClass: "btn-danger"
+        },
+        3: {
+            title: "Turned off",
+            func: null,
+            styleClass: "btn-warning"
+        }
+    },
+    servers: {},
+    executeFunction: function(server_id, name, success_state) {
+        $.getJSON("/server/" + server_id + "/" + name, function(response) {
+            if (response.success) {
+                too.servers[server_id].state = success_state;
+                too.updateView();
+            } else {
+                console.error("Error for response: ", response);
+            }
+        });
+    },
+    updateState: function() {
+        $.getJSON("/status", function(response) {
+            for (let i = 0; i < response.length; i++) {
+                let server = response[i];
+                too.servers[server.pimatic_id] = server;
+            }
+            too.updateView();
+        });
+    },
+    updateView: function() {
+        let contentDiv = $('#content');
+        contentDiv.html('');
+        for (id in too.servers) {
+            contentDiv.append(too.createElementForServer(too.servers[id]));
+        }
+    },
+    createElementForServer: function(server) {
+        let serverElement = $('<div class="server"></div>');
+
+        serverElement.append('<div class="server-state-bubble bubble_' + server.state + '"></div>');
+        serverElement.append('<div class="server-name">' + server.name + '</div>');
+        serverElement.append(too.createButtonForServer(server));
+        
+        return serverElement;
+    },
+    createButtonForServer: function(server) {
+        let state = too.serverStates[server.state];
+        let button = $('<button id="button_' + server.pimatic_id + '" class="btn btn-lg ' + state.styleClass + '" type="button"></button>');
+        button.prop("disabled", !state.func);
+        button.html(state.title);
+        if (state.func) {
+            button.click(function() {
+                state.func.call(this, server.pimatic_id);
+            })
+        }
+
+        
+        return button;
+    },
+}
---
+++ turnOnOff.py
@@ -0,0 +1,152 @@
+from flask import Flask, render_template, jsonify, redirect, request, url_for, make_response
+import requests
+import json
+import os.path
+from paramiko.client import SSHClient, AutoAddPolicy
+
+CHECK_TIMEOUT_SECONDS = 1.0
+
+app = Flask(__name__)
+app.debug = True
+
+PIMATIC_CONFIG = None
+SERVERS = {}
+PASSWORD = None
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, "settings.json")) as settings_file:
+    settings = json.load(settings_file)
+    PIMATIC = settings['pimatic']
+    PASSWORD = settings.get('password')
+    
+    for server in settings['servers']:
+        SERVERS[server['pimatic_id']] = server
+
+
+class Status:
+    OFFLINE = 0
+    TURNED_ON = 1
+    ONLINE = 2
+    TURNED_OFF = 3
+
+
+@app.route('/status')
+def check_status():
+    for server in SERVERS.values():
+        check_online(server)
+        
+    server_list = [server for server in SERVERS.values()]
+
+    return jsonify(server_list)
+
+
+def set_server_state(server, state):
+    server['state'] = state
+    if state in (Status.OFFLINE, Status.ONLINE):
+        server['last_clean_state'] = state
+        
+    return state
+
+
+def server_not_found():
+    return jsonify({
+        "success": False,
+        "error": "Server not found"
+    }), 404
+
+
+def check_online(server):
+    # First check pimatic state
+    pimatic_url = '%s/api/variables/%s.state' % (PIMATIC['host'], server['pimatic_id'])
+    response = requests.get(pimatic_url, auth=(PIMATIC['username'], PIMATIC['password']))
+    
+    json_response = response.json()
+
+    state = json_response['variable']['value']
+    
+    if state == 0:
+        return set_server_state(server, Status.OFFLINE)
+        
+    # Switch is on, check url
+    try:
+        response = requests.get(server['check_address'], timeout=CHECK_TIMEOUT_SECONDS)
+        print("Request: %s, Taken: %s ms" % (server['check_address'], str(response.elapsed.microseconds / 1000)))
+        return set_server_state(server, Status.ONLINE)
+    except Exception as e:
+        print(e)
+        if server.get('last_clean_state', None) == Status.ONLINE:
+            return set_server_state(server, Status.TURNED_OFF)
+        else:
+            return set_server_state(server, Status.TURNED_ON)
+
+    
+@app.route('/server/<id>/turnOn')
+def turn_on(id):
+    server = SERVERS.get(id)
+    if not server:
+        return server_not_found()
+        
+    pimatic_url = '%s/api/device/%s/turnOn' % (PIMATIC['host'], server['pimatic_id'])
+    response = requests.get(pimatic_url, auth=(PIMATIC['username'], PIMATIC['password']))
+    
+    return response.text
+
+
+@app.route('/server/<id>/turnOff')
+def turn_off(id):
+    server = SERVERS.get(id)
+    
+    if not server:
+        return server_not_found()
+
+    exit_status = -1
+
+    ssh_settings = server['ssh']
+    client = SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(AutoAddPolicy())
+    client.connect(ssh_settings['address'], username=ssh_settings['username'], password=ssh_settings['password'])
+    stdin, stdout, stderr = client.exec_command('shutdown -p now')
+
+    #print("stdout: " + str(stdout.readlines()))
+    #print("stderr: " + str(stderr.readlines()))
+
+    exit_status = stdout.channel.recv_exit_status()
+    print("Shutdown, exit status: %s" % exit_status)
+
+    client.close()
+    
+    return jsonify({
+        "success": exit_status == 0
+    })
+    
+
+@app.route('/login', methods = ['GET'])
+def login_get():
+    return render_template("login.html")
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    submitted_password = request.form.get('password')
+    print("Submitted password: %s" % submitted_password)
+    if submitted_password == PASSWORD:
+        # Password correct
+        resp = make_response(redirect(url_for('index')))
+        resp.set_cookie('password', submitted_password)
+        return resp
+    else:
+        return render_template("login.html", error_message="Falsches Passwort")
+
+
+@app.route('/')
+def index():
+    if PASSWORD and request.cookies.get('password') != PASSWORD:
+        return redirect(url_for('login_get'))
+
+    print(json.dumps(SERVERS))
+    return render_template('index.html')
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
